f830d15dc4fe298d3239f0c3e0b8cfa593b24f6a
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-05-10 16:12:31 +0900

    updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,10 +6,10 @@ const Navbar: React.FC = () => {
     <nav className="fixed w-full bg-[#f0e7db]/20 z-20 backdrop-blur">
       <div className="p-4 flex justify-center max-w-2xl mx-auto">
         <div className="space-x-7">
-          <Link href="/">Home</Link>
-          <Link href="/resume">Resume</Link>
-          <Link href="/projects">Projects</Link>
-          <Link href="/posts">Posts</Link>
+          <Link href="/" className="hover:underline transition">Home</Link>
+          <Link href="/resume" className="hover:underline transition">Resume</Link>
+          <Link href="/projects" className="hover:underline transition">Projects</Link>
+          <Link href="/posts" className="hover:underline transition">Posts</Link>
         </div>
       </div>
     </nav>
