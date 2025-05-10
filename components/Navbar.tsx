607bce59ffa1f981602ae2878af478d39da11b7c
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-05-10 13:53:47 +0900

    changed navbar color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="fixed w-full bg-white/25 z-20 backdrop-blur">
+    <nav className="fixed w-full bg-[#f0e7db]/20 z-20 backdrop-blur">
       <div className="p-4 flex justify-center max-w-2xl mx-auto">
         <div className="space-x-7">
           <Link href="/">Home</Link>
