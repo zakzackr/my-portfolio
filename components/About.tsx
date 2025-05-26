c255cac1a8676202de97c78801d8cbbe37f8d116
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-05-26 11:11:33 +0900

    typos

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,7 @@ const About: React.FC = () => {
         <p className="mt-5">Connect with me on {' '}
             <a href="https://github.com/zakzackr" className="underline decoration-underline-orange underline-offset-1 border-b border-transparent transition-colors hover:border-underline-orange">GitHub</a>{', '}
             <a href="https://qiita.com/shibainuu" className="underline decoration-underline-orange underline-offset-1 border-b border-transparent transition-colors hover:border-underline-orange">Qiita</a>{', and '}
-            <a href="https://zenn.dev/shibainuu" className="underline decoration-underline-orange underline-offset-1 border-b border-transparent transition-colors hover:border-underline-orange">Zenn</a>{', and '}
+            <a href="https://zenn.dev/shibainuu" className="underline decoration-underline-orange underline-offset-1 border-b border-transparent transition-colors hover:border-underline-orange">Zenn</a>{'!'}
         </p>
     </section>
   );
