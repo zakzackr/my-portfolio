5fe9f2aa3e5f875bfca0837dc478a8f7bc73ec5f
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-07-20 17:35:00 +0900

    fix favicon display in browser tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Ryuichi Kozaki",
   description: "Portfolio",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
